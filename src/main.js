2d8242fe2bb254d021e55e4ee7c3cbfa2cca9f44
z <z>
2019-02-26 19:46:47 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import {post,fetch,patch,put} from './components/base/request'
+//定义全局变量
+Vue.prototype.$post=post;
+Vue.prototype.$fetch=fetch;
+Vue.prototype.$patch=patch;
+Vue.prototype.$put=put;
 Vue.config.productionTip = false
 
 new Vue({
