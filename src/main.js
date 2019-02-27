c4ee9735718c4136e1542ca52afc446b12a00dc0
z <z>
2019-02-27 16:49:58 +0800

    VUX

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+// import FastClick from 'fastclick'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import {post,fetch,patch,put} from './components/base/request'
+const FastClick = require('fastclick') //移除移动端点击延迟
+FastClick.attach(document.body)
+
 //定义全局变量
 Vue.prototype.$post=post;
 Vue.prototype.$fetch=fetch;
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+
+const path = require('path')
+const vuxLoader = require('vux-loader')
 module.exports = {
     // 基本路径
     baseUrl: '/',
@@ -5,21 +8,22 @@ module.exports = {
     outputDir: 'dist',
     // eslint-loader 是否在保存的时候检查
     lintOnSave: true,
-    // webpack配置
-    chainWebpack: () => { },
-    configureWebpack: () => { },
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: true,
+    runtimeCompiler: true,// 包含运行时编译器的 Vue 构建版本
     // css相关配置
     css: {
-        // 是否使用css分离插件 ExtractTextPlugin
-        extract: true,
-        // 开启 CSS source maps?
-        sourceMap: false,
-        // css预设器配置项
-        loaderOptions: {},
-        // 启用 CSS modules for all css / pre-processor files.
-        modules: false
+        // 配置高于chainWebpack中关于css loader的配置
+        modules: true, // 是否开启支持‘foo.module.css’样式
+        extract: true, // 是否使用css分离插件 ExtractTextPlugin，采用独立样式文件载入，不采用<style>方式内联至html文件中
+        sourceMap: false, // 是否在构建样式地图，false将提高构建速度
+        loaderOptions: { // css预设器配置项
+            css: {
+                localIdentName: '[name]-[hash]',
+                camelCase: 'only'
+            },
+            stylus: {}
+        }
     },
     parallel: require('os').cpus().length > 1,
     pwa: {},
@@ -41,6 +45,36 @@ module.exports = {
             }
         },                                           // 配置跨域处理,只有一个代理
         before: app => { }
+    },
+    // webpack配置
+    chainWebpack: () => { },
+    // vux 相关配置,使用vux-ui
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.mode = 'production';
+        } else {
+            // 为开发环境修改配置...
+            config.mode = 'development';
+        }
+        vuxLoader.merge(config, {
+            options: {},
+            plugins: [{
+                name: 'vux-ui'
+            },{
+                name: 'duplicate-style'
+            }]
+        })
+        Object.assign(config, {
+            // 开发生产共同配置
+            resolve: {
+                extensions: ['.js', '.vue', '.json',".css"],
+                alias: {
+                    '@': path.resolve(__dirname, './src'),
+                    '@c': path.resolve(__dirname, './src/components')
+                }
+            }
+        })
     },
     // 第三方插件配置
     pluginOptions: {}
